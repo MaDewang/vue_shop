27ab08ac77733f5461b750c2e67b7b60b6924e5c
mdw <mdw@mdw.cn>
2019-11-03 14:55:17 +0800

    完成登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 Vue.use(VueRouter)
 
-const routes = []
-
+const routes = [
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
+]
 const router = new VueRouter({
   routes
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to   去哪
+  // from 从哪来
+  // next() 放行 
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
